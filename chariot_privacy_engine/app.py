6c70486deed990587c08ed4c0f74389071c1ca0b
George Theofilis <g.theofilis@clmsuk.com>
2019-02-05 11:07:49 +0200

    🎨💥Connection to the brokers

--- chariot_privacy_engine/app.py
+++ chariot_privacy_engine/app.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 
 import os
 import uuid
@@ -8,26 +7,24 @@ import gmqtt
 import asyncio
 import signal
 
-import uuid
-import json
 import falcon
 import falcon_jsonify
 
 from chariot_privacy_engine.resources import MessageResource
 from chariot_privacy_engine.engine import Engine
 from chariot_base.model import Message, DataPointFactory
-from chariot_base.connector import LocalConnector
+from chariot_base.utilities import open_config_file
+from chariot_base.connector import LocalConnector, create_client
 
 
 class SouthboundConnector(LocalConnector):
     def __init__(self, options):
         super(SouthboundConnector, self).__init__()
         self.engine = None
-        self.connector = None
         self.point_factory = DataPointFactory(options['database'], options['table'])
 
     def on_message(self, client, topic, payload, qos, properties):
-        point = self.to_data_point(payload, topic)
+        print(topic, payload)
 
     def set_up_engine(self, engine):
         self.engine = engine
@@ -37,7 +34,6 @@ class NorthboundConnector(LocalConnector):
     def __init__(self, options):
         super(NorthboundConnector, self).__init__()
         self.engine = None
-        self.connector = None
         self.point_factory = DataPointFactory(options['database'], options['table'])
 
     def set_up_engine(self, engine):
@@ -54,24 +50,19 @@ def ask_exit(*args):
 async def main(args=None):
     engine = Engine()
 
-    filename = None
-    for name in ['./config.json', './tests/config.json']:
-        if os.path.isfile(name):
-            filename = name
-
-    if filename is None:
-        raise Exception('Configuration file is not exists')
-
-    with open('./tests/config.json', 'r') as read_file:
-        opts = json.load(read_file)
+    opts = open_config_file()
 
     options_engine = opts['engine']['privacy']
 
     southbound = SouthboundConnector(options_engine)
     southbound.set_up_engine(engine)
+    client_south = create_client(opts['brokers']['southbound'])
+    southbound.register_for_client(client_south)
 
     northbound = NorthboundConnector(options_engine)
     northbound.set_up_engine(engine)
+    client_north = create_client( opts['brokers']['northbound'])
+    northbound.register_for_client(client_north)
 
     engine.inject(southbound, northbound)
     engine.start()
