ac3f1ea4c9c9516c1c9a85e2dbc94c978fc2a71b
George Theofilis <theofilis.g@gmail.com>
2019-03-19 18:31:26 +0200

    Bump version: 0.4.3 → 0.5.0

--- chariot_privacy_engine/__init__.py
+++ chariot_privacy_engine/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """George Theofilis"""
 __email__ = 'g.theofilis@clmsuk.com'
-__version__ = '0.4.3'
+__version__ = '0.5.0'
--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/theofilis/chariot_privacy_engine',
-    version='0.4.3',
+    version='0.5.0',
     zip_safe=False,
 )
