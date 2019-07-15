0d8f100cf8f556e08398c43036a0576f1e27e986
George Theofilis <g.theofilis@clmsuk.com>
2019-07-15 17:44:17 +0300

    Change rule name

--- chariot_privacy_engine/filter/rules.py
+++ chariot_privacy_engine/filter/rules.py
@@ -12,7 +12,7 @@ from chariot_base.utilities import has_write_right, has_read_right
 class RsaRuleFilter(object):
 
     def __init__(self, engine):
-        self.human_name = 'rsa_rule_filter'
+        self.human_name = 'encryption_rule_filter'
         self.engine = engine
 
     def do(self, message, span):
@@ -51,7 +51,7 @@ class RsaRuleFilter(object):
         return message
 
     def ecdh_encyption(self, span, params, message):
-        encrypt_span = self.engine.start_span('ecdh_encrypt_%s' % self.human_name, span)
+        encrypt_span = self.engine.start_span(f'ecdh_encrypt_{self.human_name}', span)
         msg = 'The ECDH encyption scheme is not supported yet.'
         alert = Alert(self.human_name, msg, 100)
         alert.sensor_id = message.sensor_id
@@ -60,11 +60,11 @@ class RsaRuleFilter(object):
         return message
 
     def rsa_encyption(self, span, params, message):
-        encrypt_span = self.engine.start_span('rsa_encrypt_%s' % self.human_name, span)
+        encrypt_span = self.engine.start_span(f'rsa_encrypt_{self.human_name}', span)
         public_key = RSA.importKey(base64.b64decode(params['pubkey']))
         encrypted_msg = public_key.encrypt(message.value.encode('utf-8'), 32)[0]
         encoded_msg = base64.b64encode(encrypted_msg).decode('utf-8')
-        logging.debug('Encrypted message: %s' % encoded_msg)
+        logging.debug(f'Encrypted message: {encoded_msg}')
         message.value = encoded_msg
         self.engine.close_span(encrypt_span)
         return message
