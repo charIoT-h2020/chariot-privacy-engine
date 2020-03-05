4c32316cbeab618da438e2f8d4630fd00989de76
George Theofilis <theofilis.g@gmail.com>
2020-03-05 15:08:59 +0000

    Mute alert for specific sensor

--- chariot_privacy_engine/engine/engine.py
+++ chariot_privacy_engine/engine/engine.py
@@ -33,13 +33,7 @@ class Engine(Traceable):
             RsaRuleFilter(self)
         ]
 
-        self.muted_options = self.options.get('mute', {})
-        for _inspector in self.inspectors:
-            if _inspector.human_name not in self.muted_options:
-                self.muted_options[_inspector.human_name] = False
-        for _filters in self.filters:
-            if _filters.human_name not in self.muted_options:
-                self.muted_options[_filters.human_name] = False
+        self.prepare_global_mute_options()
 
     def inject(self, southbound, northbound):
         self.southbound = southbound
@@ -72,25 +66,31 @@ class Engine(Traceable):
     def inspect(self, message, child_span):
         for _inspector in self.inspectors:
             if self.is_not_muted(_inspector.human_name):
-                span = self.start_span(f'filter_{_inspector.human_name}', child_span)
-                _inspector.check(message, span)
-                self.close_span(span)
+                if self.is_not_muted_for_sensor(_inspector.human_name, message):
+                    span = self.start_span(f'filter_{_inspector.human_name}', child_span)
+                    _inspector.check(message, span)
+                    self.close_span(span)
+                else:
+                    logging.debug(f'Rule {_inspector.human_name} is muted for sensor "{message.sensor_id}"')
             else:
                 logging.debug(f'Rule {_inspector.human_name} is muted')
 
     def filter(self, message, child_span):
         for _filter in self.filters:
             if self.is_not_muted(_filter.human_name):
-                span = self.start_span(f'filter_{_filter.human_name}', child_span)
-                _filter.do(message, span)
-                self.close_span(span)
+                if self.is_not_muted_for_sensor(_filter.human_name, message):
+                    span = self.start_span(f'filter_{_filter.human_name}', child_span)
+                    _filter.do(message, span)
+                    self.close_span(span)
+                else:
+                    logging.debug(f'Rule {_filter.human_name} is muted for sensor "{message.sensor_id}"')
             else:
                 logging.debug(f'Rule {_filter.human_name} is muted')
 
     def publish(self, message, span):
         m = self.inject_to_message(span, message.dict())        
         self.southbound.publish('northbound', json.dumps(m))
-        logging.debug('Publish message from "%s" to "%s"' % (message.sensor_id, message.destination))
+        logging.debug(f'Publish message from "{message.sensor_id}" to "{message.destination}"')
 
     def raise_alert(self, alert, span):
         m = json.dumps(self.inject_to_message(span, alert.dict()))
@@ -99,7 +99,7 @@ class Engine(Traceable):
 
     def get_acl(self, span, message):
         return self.iotl.acl(message.sensor_id)
-    
+
     def get_params(self, span, destination):
         return self.iotl.params(destination)
 
@@ -130,5 +130,18 @@ class Engine(Traceable):
             return True
         return datetime.now() - self.last_sync_datetime >= timedelta(seconds=self.options.get('iotl_sync_delay', 60))
 
-    def is_not_muted(self, id, message=None):
+    def is_not_muted(self, id):
         return not self.muted_options[id]
+
+    def is_not_muted_for_sensor(self, id, message):
+        muted = self.iotl.params(message.sensor_id).get('mute', {})
+        return muted.get(id, 0) == 0
+
+    def prepare_global_mute_options(self):
+        self.muted_options = self.options.get('mute', {})
+        for _inspector in self.inspectors:
+            if _inspector.human_name not in self.muted_options:
+                self.muted_options[_inspector.human_name] = False
+        for _filters in self.filters:
+            if _filters.human_name not in self.muted_options:
+                self.muted_options[_filters.human_name] = False
