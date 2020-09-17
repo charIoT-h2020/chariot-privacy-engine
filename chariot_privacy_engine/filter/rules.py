e2f86f44fa1388550edf76087477bd61b1c0510b
George Theofilis <g.theofilis@clmsuk.com>
2020-09-17 10:58:22 +0000

    Update rules.py

--- chariot_privacy_engine/filter/rules.py
+++ chariot_privacy_engine/filter/rules.py
@@ -5,6 +5,9 @@ import base64
 import logging
 
 from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+
 from chariot_base.model import Alert, Message
 from chariot_base.utilities import has_write_right, has_read_right
 
@@ -74,9 +77,29 @@ class RsaRuleFilter(object):
 
     def rsa_encyption(self, span, params, message):
         encrypt_span = self.engine.start_span(f'rsa_encrypt_{self.human_name}', span)
-        public_key = RSA.importKey(base64.b64decode(params['pubkey']))
-        encrypted_msg = public_key.encrypt(message.value.encode('utf-8'), 32)[0]
-        encoded_msg = base64.b64encode(encrypted_msg).decode('utf-8')
+
+        data = message.value.encode('utf-8')
+        base64_public_key = params['pubkey']
+
+        serialized_public_key = base64.b64decode(base64_public_key)
+
+        recipient_key = RSA.import_key(serialized_public_key)
+
+        session_key = get_random_bytes(16)
+
+        cipher_rsa = PKCS1_OAEP.new(recipient_key)
+        enc_session_key = cipher_rsa.encrypt(session_key)
+
+        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+
+        enc_session_key_d = base64.b64encode(enc_session_key).decode('utf-8')
+        nonce_d = base64.b64encode(cipher_aes.nonce).decode('utf-8')
+        tag_d = base64.b64encode(cipher_aes.nonce).decode('utf-8')
+        ciphertext_d = base64.b64encode(ciphertext).decode('utf-8')
+
+        encoded_msg = json.dumps([enc_session_key_d, nonce_d, tag_d, ciphertext_d])
+
         logging.debug(f'Encrypted message: {encoded_msg}')
         new_message = Message(message.sensor_id, encoded_msg)
         new_message.id = messages.id
